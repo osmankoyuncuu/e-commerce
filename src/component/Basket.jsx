89c7f9874cd4bf1ed4017844a106e22815455359
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-11-25 12:32:53 +0300

    add plus and minus func

--- src/component/Basket.jsx
+++ src/component/Basket.jsx
@@ -4,11 +4,34 @@ import Checkbox from "@mui/material/Checkbox";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import IconButton from "@mui/material/IconButton";
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import { useDispatch } from "react-redux";
+import { deleteShopping } from "../features/shoppingSlice";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Basket = ({ product }) => {
-  const { image, title, price } = product;
+  const dispatch = useDispatch();
+  const { image, title, price, id } = product;
+  const [piece, setPiece] = useState(1);
+  const [itemTotal, setItemTotal] = useState(0);
+
+  const handlePieceMinus = () => {
+    if (piece > 1) {
+      setPiece(piece - 1);
+    } else {
+      dispatch(deleteShopping(product));
+    }
+  };
+
+  const handleRemove = () => {
+    dispatch(deleteShopping(product));
+  };
+  useEffect(() => {
+    setItemTotal(price * piece);
+  }, [piece]);
+
   return (
     <Box
       sx={{
@@ -35,10 +58,10 @@ const Basket = ({ product }) => {
         <Box>
           <img src={image} alt="" width="100px" />
         </Box>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: ".5rem" }}>
           <Typography variant="h6">{title}</Typography>
           <Typography variant="h5" color="primary">
-            $ {price}
+            $ {itemTotal}
           </Typography>
         </Box>
       </Box>
@@ -53,11 +76,12 @@ const Basket = ({ product }) => {
             "&:hover": { color: "primary.main" },
             transition: "all .2s",
           }}
+          onClick={() => setPiece(piece + 1)}
         >
           <AddCircleOutlineIcon />
         </IconButton>
 
-        <Typography>1</Typography>
+        <Typography>{piece}</Typography>
         <IconButton
           size="large"
           aria-haspopup="true"
@@ -66,9 +90,17 @@ const Basket = ({ product }) => {
             "&:hover": { color: "primary.main" },
             transition: "all .2s",
           }}
+          onClick={handlePieceMinus}
         >
           <RemoveCircleOutlineIcon />
         </IconButton>
+        <Button
+          variant="contained"
+          sx={{ mt: 1 }}
+          onClick={() => handleRemove(id)}
+        >
+          Remove
+        </Button>
       </Box>
     </Box>
   );
