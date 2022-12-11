d95d2c9a92123b990c07222f19f63ef8a2d7ba11
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-12-11 18:32:02 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
       },
     },
   });
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,12 @@
 import Box from "@mui/material/Box";
 import ProductCard from "../component/ProductCard";
 import { useSelector } from "react-redux";
-import { getProduct } from "../features/productSlice";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import Grid from "@mui/material/Grid";
 import CategoryFilter from "../component/CategoryFilter";
-import {
-  favoriteListenerFirebase,
-  shoppingListenerFirebase,
-  userObserver,
-} from "../utils/firebase";
-import { shoppingListener } from "../features/shoppingSlice";
-import { clearCurrentUser, createCurrentUser } from "../features/authSlice";
 import { minHeight } from "../styles/globalStyle";
-import { favoriteListener } from "../features/favoriteSlice";
 
 const Home = () => {
   const { filterProductList } = useSelector((state) => state.product);
-  const { currentUser } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const { favoriteList } = useSelector((state) => state.favorite);
-  console.log(currentUser);
-  console.log(favoriteList);
-  useEffect(() => {
-    userObserver(dispatch, createCurrentUser, clearCurrentUser);
-    favoriteListenerFirebase(dispatch, favoriteListener, currentUser);
-    shoppingListenerFirebase(dispatch, shoppingListener, currentUser);
-    dispatch(getProduct());
-  }, []);
 
   return (
     <Box sx={minHeight}>
